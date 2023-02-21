96e68ce0f401a8cd1659a86565f96d4f06d47c84
Swayam Kaul <swayameron@gmail.com>
2023-02-21 11:44:20 +0530

    normalisation fixed

--- pos/src/main/java/com/increff/pos/util/NormaliseUtil.java
+++ pos/src/main/java/com/increff/pos/util/NormaliseUtil.java
@@ -8,11 +8,14 @@ import java.util.List;
 
 public class NormaliseUtil {
     public static void normalise(BrandForm brandForm){
-        brandForm.setBrand(StringUtil.toLowerCase(brandForm.getBrand()));
-        brandForm.setCategory(StringUtil.toLowerCase(brandForm.getCategory()));
+        brandForm.setBrand(StringUtil.toLowerCase(brandForm.getBrand()).trim());
+        brandForm.setCategory(StringUtil.toLowerCase(brandForm.getCategory()).trim());
     }
-    public static void normalise(ProductForm p) {
-        p.setName(StringUtil.toLowerCase(p.getName()));
+    public static void normalise(ProductForm productForm) {
+        productForm.setName(StringUtil.toLowerCase(productForm.getName()).trim());
+        productForm.setBarCode(StringUtil.toLowerCase(productForm.getBarCode()).trim());
+        productForm.setBrand(StringUtil.toLowerCase(productForm.getBrand()).trim());
+        productForm.setCategory(StringUtil.toLowerCase(productForm.getCategory()).trim());
     }
 
     public static void normalise(OrderItemForm orderItemForm) {
